fd6cf996f0c7890a9c099dff651bda23e7a29059
AdamDewandaru <adamdewandaru@gmail.com>
2022-06-25 21:51:51 +0700

    uas

---
+++ lib/location/maps.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:kopi/location/google_maps_widget.dart';
+import 'package:kopi/location/user_location.dart';
+import 'package:kopi/location/location_provider.dart';
+import 'package:kopi/location/location_service.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:provider/provider.dart';
+import 'dart:async';
+
+class Maps extends StatefulWidget {
+  @override
+  State<Maps> createState() => MapSampleState();
+}
+
+class MapSampleState extends State<Maps> {
+  Completer<GoogleMapController> _controller = Completer();
+
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
+  static final CameraPosition _kLake = CameraPosition(
+      bearing: 192.8334901395799,
+      target: LatLng(37.43296265331129, -122.08832357078792),
+      tilt: 59.440717697143555,
+      zoom: 19.151926040649414);
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: GoogleMap(
+        mapType: MapType.terrain,
+        initialCameraPosition: _kGooglePlex,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: _goToTheLake,
+        label: Text('To the lake!'),
+        icon: Icon(Icons.directions_boat),
+      ),
+    );
+  }
+
+  Future<void> _goToTheLake() async {
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
+  }
+}
